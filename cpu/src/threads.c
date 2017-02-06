e075efe30a471f0a1e7f9c56143ff76685844ebb
huzcn <zhouhchn@gmail.com>
2017-02-06 02:22:21 -0800

    finish thread creation and thread contextswitch

--- cpu/src/threads.c
+++ cpu/src/threads.c
@@ -9,13 +9,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
+#include <unistd.h>
 #include "utils.h"
 #include "threads.h"
+
+static int fd[2];	// pipe descriptors will be stored in this array.
 
 /* Function invoked for thread performance measurements
  * Just returns
  */
 void* thread_function() {
+	pthread_exit(NULL);
+}
+
+/* Function invoked for thread context switch measurements
+ * New thread will write the END value of timer to the pipe
+ */
+void* thread_function_2(void *p) {
+	uint64_t end;
+	END_RDTSCP(end);
+	write(fd[1], (void*)&end, sizeof(uint64_t));
 	pthread_exit(NULL);
 }
 
@@ -63,7 +76,7 @@ void measure_thread_creation(int iterations) {
         fprintf(fp, "%"PRIu64"\n", ticks[i]);
     }
 
-	uint64_t average = calc_average(ticks, iterations, 1000, 10000, 5000);
+	uint64_t average = calc_average(ticks, iterations, 0, 10000, 5000);
 	printf("THREAD_CREATION : Average cycles = %"PRIu64"\n\n", average);
 
 	fflush(NULL);	// flush everything in the buffer, otherwise other threads
@@ -74,4 +87,41 @@ void measure_thread_creation(int iterations) {
 }
 
 void measure_thread_switch(int iterations) {
+
+	uint64_t start = 0, end = 0;
+	uint64_t* ticks = (uint64_t*) malloc (sizeof(uint64_t) * iterations);
+	memset(ticks, 0, iterations * sizeof(uint64_t));
+    FILE* fp = fopen("logs/thread_switch.txt", "w+");
+
+	if(pipe(fd) != 0) {
+		printf("fail to create pipe in thread switch\n");
+		exit(1);
+	}
+
+	pthread_t thread;
+
+	for(int i=0; i<iterations; ++i) {
+
+		pthread_create(&thread, NULL, thread_function_2, NULL);
+
+		START_RDTSC(start);
+
+		pthread_join(thread, NULL);
+		read(fd[0], (void*)&end, sizeof(uint64_t));
+		
+		ticks[i] = (end - start);
+	}
+
+    for(int i=0; i<iterations; ++i) {
+        fprintf(fp, "%"PRIu64"\n", ticks[i]);
+    }
+
+	uint64_t average = calc_average(ticks, iterations, 0, 10000, 5000);
+	printf("THREAD_SWITCH : Average cycles = %"PRIu64"\n\n", average);
+
+	fflush(NULL);	// flush everything in the buffer, otherwise other threads
+					// will share the same buffer, which may go wrong in redirection
+
+	fclose(fp);
+	free(ticks);
 }
