e70f0a8eab514991cc68dddae5a37469e8aeacf9
huzcn <zhouhchn@gmail.com>
2017-03-15 02:19:55 -0700

    update build_all.sh and run_all.sh

---
+++ build_all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "========================="
+echo "Building source files"
+echo "========================="
+
+echo ""
+echo "-------------------------"
+echo "CPU"
+echo "-------------------------"
+cd cpu
+./build.sh
+cd ../
+
+echo ""
+echo "-------------------------"
+echo "MEMORY"
+echo "-------------------------"
+echo ""
+cd mem
+./build.sh
+cd ../
+echo ""
+
+echo ""
+echo "-------------------------"
+echo "NETWORK"
+echo "-------------------------"
+echo ""
+cd network
+./build.sh
+cd ../
+echo ""
---
+++ run_all.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage ./run <num_iterations>"
+	exit
+fi
+
+echo ""
+echo "========================="
+echo "Running experiments"
+echo "========================="
+echo ""
+
+echo ""
+echo "-------------------------"
+echo "CPU"
+echo "-------------------------"
+echo ""
+cd cpu
+./run.sh $1
+cd ..
+
+echo ""
+echo "-------------------------"
+echo "MEMORY"
+echo "-------------------------"
+echo ""
+cd mem
+./run.sh
+cd ..
+
+echo ""
+echo "-------------------------"
+echo "NETWORK"
+echo "-------------------------"
+echo ""
+cd network
+./run.sh
+cd ..
+
+echo ""
+echo "========================="
+echo "Done!"
+echo "========================="
+echo ""
