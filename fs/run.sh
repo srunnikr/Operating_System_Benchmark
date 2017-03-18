e8ad40175a2d5a03747882d29755d5c56077b45a
huzcn <zhouhchn@gmail.com>
2017-03-18 12:00:07 -0700

    update run.sh

--- fs/run.sh
+++ fs/run.sh
@@ -1,55 +1,53 @@
 #!/bin/bash
 
-python generate_files.py
-
 echo ""
 echo "Test for 1KB file"
-./bin/fs "$PWD/testfiles/f1.txt"
+./build/bin/fs "$PWD/testfiles/f1.txt"
 
 echo ""
 echo "Test for 2KB file"
-./bin/fs "$PWD/testfiles/f2.txt"
+./build/bin/fs "$PWD/testfiles/f2.txt"
 
 echo ""
 echo "Test for 4KB file"
-./bin/fs "$PWD/testfiles/f4.txt"
+./build/bin/fs "$PWD/testfiles/f4.txt"
 
 echo ""
 echo "Test for 8KB file"
-./bin/fs "$PWD/testfiles/f8.txt"
+./build/bin/fs "$PWD/testfiles/f8.txt"
 
 echo ""
 echo "Test for 16KB file"
-./bin/fs "$PWD/testfiles/f16.txt"
+./build/bin/fs "$PWD/testfiles/f16.txt"
 
 echo ""
 echo "Test for 32KB file"
-./bin/fs "$PWD/testfiles/f32.txt"
+./build/bin/fs "$PWD/testfiles/f32.txt"
 
 echo ""
 echo "Test for 64KB file"
-./bin/fs "$PWD/testfiles/f64.txt"
+./build/bin/fs "$PWD/testfiles/f64.txt"
 
 echo ""
 echo "Test for 128KB file"
-./bin/fs "$PWD/testfiles/f128.txt"
+./build/bin/fs "$PWD/testfiles/f128.txt"
 
 echo ""
 echo "Test for 256KB file"
-./bin/fs "$PWD/testfiles/f256.txt"
+./build/bin/fs "$PWD/testfiles/f256.txt"
 
 echo ""
 echo "Test for 512KB file"
-./bin/fs "$PWD/testfiles/f512.txt"
+./build/bin/fs "$PWD/testfiles/f512.txt"
 
 echo ""
 echo "Test for 1MB file"
-./bin/fs "$PWD/testfiles/f1024.txt"
+./build/bin/fs "$PWD/testfiles/f1024.txt"
 
 echo ""
 echo "Test for 2MB file"
-./bin/fs "$PWD/testfiles/f2048.txt"
+./build/bin/fs "$PWD/testfiles/f2048.txt"
 
 echo ""
 echo "Test for 4MB file"
-./bin/fs "$PWD/testfiles/f4096.txt"
+./build/bin/fs "$PWD/testfiles/f4096.txt"
